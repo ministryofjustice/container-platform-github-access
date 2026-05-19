fc48f8530d71b2691955013d66ca8db307aa9e59
Folarin Oyenuga <157374556+FolarinOyenuga@users.noreply.github.com>
2026-05-19 16:32:43 +0100

    feat: add Container Platform team as admin on repos (#14)

--- github-repositories.tf
+++ github-repositories.tf
@@ -5,7 +5,7 @@ locals {
       description  = "Container Platform GitHub Access"
       has_projects = true
       access = {
-        admins  = [data.github_team.cloud_platform_engineers.id]
+        admins  = [module.github_team.id, data.github_team.cloud_platform_engineers.id]
         pushers = [data.github_team.all_org_members.id]
       }
     }
