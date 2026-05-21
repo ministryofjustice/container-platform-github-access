34ce33965edb4a2e4cdc933363f5c5e9a0a7204e
Folarin Oyenuga <157374556+FolarinOyenuga@users.noreply.github.com>
2026-05-21 14:41:06 +0100

    Import user guide (#29)
    * chore: remove template import blocks after successful import
    
    * feat: import container-platform-user-guide

--- imports.tf
+++ imports.tf
@@ -1,24 +1,14 @@
 import {
-  to = module.github_repositories["container-platform-terraform-template"].github_repository.this
-  id = "container-platform-terraform-template"
+  to = module.github_repositories["container-platform-user-guide"].github_repository.this
+  id = "container-platform-user-guide"
 }
 
 import {
-  to = module.github_repositories["container-platform-terraform-template"].github_repository_dependabot_security_updates.this
-  id = "container-platform-terraform-template"
+  to = module.github_repositories["container-platform-user-guide"].github_repository_dependabot_security_updates.this
+  id = "container-platform-user-guide"
 }
 
 import {
-  to = module.github_repositories["container-platform-terraform-template"].github_repository_ruleset.main
-  id = "container-platform-terraform-template:14924091"
-}
-
-import {
-  to = module.github_repositories["container-platform-terraform-template"].github_team_repository.admin["1512717"]
-  id = "1512717:container-platform-terraform-template"
-}
-
-import {
-  to = module.github_repositories["container-platform-terraform-template"].github_team_repository.admin["12737405"]
-  id = "12737405:container-platform-terraform-template"
+  to = module.github_repositories["container-platform-user-guide"].github_team_repository.admin["1512717"]
+  id = "1512717:container-platform-user-guide"
 }
