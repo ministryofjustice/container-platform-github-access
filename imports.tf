dae02ffdfb281ce388edb2386c0240f43cc9930c
Folarin Oyenuga <157374556+FolarinOyenuga@users.noreply.github.com>
2026-05-22 11:25:42 +0100

    Import starter pack (#35)
    * chore: remove external-dns import blocks after successful import
    
    * feat: import container-platform-terraform-starter-pack

--- imports.tf
+++ imports.tf
@@ -1,14 +1,4 @@
 import {
-  to = module.github_repositories["container-platform-terraform-external-dns"].github_repository.this
-  id = "container-platform-terraform-external-dns"
-}
-
-import {
-  to = module.github_repositories["container-platform-terraform-external-dns"].github_team_repository.admin["1512717"]
-  id = "1512717:container-platform-terraform-external-dns"
-}
-
-import {
-  to = module.github_repositories["container-platform-terraform-external-dns"].github_team_repository.admin["12737405"]
-  id = "12737405:container-platform-terraform-external-dns"
+  to = module.github_repositories["container-platform-terraform-starter-pack"].github_repository.this
+  id = "container-platform-terraform-starter-pack"
 }
