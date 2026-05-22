6986fc74dae32c5b32251a432d62918c675c5466
Folarin Oyenuga <157374556+FolarinOyenuga@users.noreply.github.com>
2026-05-22 11:13:19 +0100

    feat: import container-platform-terraform-external-dns (#34)

--- github-repositories.tf
+++ github-repositories.tf
@@ -46,6 +46,15 @@ locals {
         pushers = [data.github_team.all_org_members.id]
       }
     }
+    container_platform_terraform_external_dns = {
+      name         = "container-platform-terraform-external-dns"
+      description  = "External DNS Terraform module for the Container Platform"
+      has_projects = true
+      access = {
+        admins  = [module.github_team.id, data.github_team.cloud_platform_engineers.id, data.github_team.webops.id]
+        pushers = [data.github_team.all_org_members.id]
+      }
+    }
     container_platform_user_guide = {
       name          = "container-platform-user-guide"
       description   = "User documentation for the Container Platform at the Ministry of Justice"
