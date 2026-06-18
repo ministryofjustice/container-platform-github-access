ce725801dbc7a244f3ad269323f9503aba409a21
dan-criddle <dan.criddle@justice.gov.uk>
2026-06-18 12:20:55 +0100

    Fix vad characters

--- github-repositories.tf
+++ github-repositories.tf
@@ -104,17 +104,16 @@ locals {
         pushers = [data.github_team.all_org_members.id]
       }
     }
-    container-platform-integration-tests = {
-        name         = "container-platform-integration-tests"
-        description  = "Integration tests for the Container Platform clusters"
+    container-platform-integration-tests = {    
+      name                 = "container-platform-integration-tests"
+      description   = "Integration tests for the Container Platform clusters"
       has_projects = true
-        use_template = true
-        access = {
-          admins  = [module.github_team.id, data.github_team.cloud_platform_engineers.id, data.github_team.webops.id]
-          pushers = [data.github_team.all_org_members.id]
-        }
-      }
-    }
+      use_template = true
+      access = {
+        admins   = [module.github_team.id, data.github_team.cloud_platform_engineers.id, data.github_team.webops.id]
+        pushers = [data.github_team.all_org_members.id]
+      }
+    }
   }
 }
 
