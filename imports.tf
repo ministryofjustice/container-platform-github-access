c1dfb4a7e385f113810c64afbc420443f07b49ea
dan-criddle <dan.criddle@justice.gov.uk>
2026-06-18 12:43:08 +0100

    Add container-platform-integration-tests repository (#53)
    * Add container-platform-integration-tests repository
    
    Added a new repository configuration for integration tests.
    
    Signed-off-by: dan-criddle <dan.criddle@justice.gov.uk>
    
    * Add container-platform-integration-tests
    
    * Fix vad characters
    
    * Fix formatting
    
    * Fix formatting
    
    ---------
    
    Signed-off-by: dan-criddle <dan.criddle@justice.gov.uk>

---
+++ imports.tf
@@ -0,0 +1,5 @@
+# Always import the repository itself
+import {
+  to = module.github_repositories["container-platform-integration-tests"].github_repository.this
+  id = "container-platform-integration-tests"
+}
