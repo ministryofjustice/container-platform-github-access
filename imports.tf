52e7bf1213a305d9481b2755729f6a3c5fd8b495
dan-criddle <dan.criddle@justice.gov.uk>
2026-06-18 16:47:22 +0100

    Cleanup imports.tf after container-platform-integration-test import (#54)
    Clup imports.tf after container-platform-integration-test import
    
    Signed-off-by: dan-criddle <dan.criddle@justice.gov.uk>

--- imports.tf
+++
@@ -1,5 +0,0 @@
-# Always import the repository itself
-import {
-  to = module.github_repositories["container-platform-integration-tests"].github_repository.this
-  id = "container-platform-integration-tests"
-}
