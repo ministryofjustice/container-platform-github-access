97faf9362222c31527068431a189f7c865012cce
Ky <ky.truong@digital.justice.gov.uk>
2026-06-26 15:48:04 +0100

    add-cert-manager-repo

--- github-repositories.tf
+++ github-repositories.tf
@@ -127,6 +127,15 @@ locals {
         pushers = [data.github_team.all_org_members.id]
       }
     }
+    container_platform_terraform_cert_manager = {
+      name         = "container-platform-terraform-cert-manager"
+      description  = "Cert Manager Terraform module for the Container Platform"
+      use_template = true
+      access = {
+        admins  = [module.github_team.id, data.github_team.cloud_platform_engineers.id, data.github_team.cloud_platform.id]
+        pushers = [data.github_team.all_org_members.id]
+      }
+    }    
   }
 }
 
